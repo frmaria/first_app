17d71efb7f60a6d5415703ce3b3be32431b196d7
frmaria <mariabrilaki@gmail.com>
2013-03-14 11:09:03 -0700

    Solving Heroku problem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '1fb36a06a7003d177588ed9b604c536cf3a8bd4d51a2e2daad312028a7a017fabf0e94ae132ea2326c080eccf86c72a2845aa7a8bef51d7a84f2086677c527d4'
